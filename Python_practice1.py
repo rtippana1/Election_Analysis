eee1acf86d0e53942ba19f4363406fc6aeef15b3
rtippana <radhika.tippanna@gmail.com>
2020-12-02 20:01:45 -0800

    commit example

---
+++ Python_practice1.py
@@ -0,0 +1,7 @@
+counties =["Arapahoe","Denver","Jefferson"]
+#if counties[1] == "Denver":
+#    print(counties[1])
+if "El Paso" in counties:
+    print("El Paso is present in list")
+else:   
+    print("El Paso is not present in list")
---
+++ ifelifelse.py
@@ -0,0 +1,13 @@
+#what is your score
+score = int(input("What is your score ?"))
+
+if score >= 90:
+    print('your grade is A .')
+elif score >= 80:
+    print('your grade is B .')
+elif score >= 70:
+    print('your score is c .')
+elif score >= 60:
+    print('your grade is D .')
+else:
+    print('your grade is F .')
